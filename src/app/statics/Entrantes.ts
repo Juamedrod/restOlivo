c18c4eeaa16b7014741282681c04640e1bab5f75
JM_Medina <juamedrod@hotmail.es>
2023-07-31 18:03:07 +0200

    FEATURE - Actualizacion Verano 2023 - Agosto - Atun brasa añadido y ajuste de precios

--- src/app/statics/Entrantes.ts
+++ src/app/statics/Entrantes.ts
@@ -5,11 +5,11 @@ export const Entrantes: Plato[] = [
   { nombre: 'plato2', alergenos: [12], precioMedia: 'X', precioEntera: '8.50' },
   { nombre: 'plato3', alergenos: [3, 4, 12], precioMedia: 'X', precioEntera: '9.00' },
   { nombre: 'plato4', alergenos: [7, 12, 6, 3], precioMedia: 'X', precioEntera: '12.00' },
-  { nombre: 'plato5', alergenos: [1, 12, 8, 10], precioMedia: 'X', precioEntera: '12.00' },
+  { nombre: 'plato5', alergenos: [1, 12, 8, 10], precioMedia: 'X', precioEntera: '14.00' },
   { nombre: 'plato6', alergenos: [1, 12, 8, 5], precioMedia: 'X', precioEntera: '14.00' },
-  { nombre: 'plato7', alergenos: [12], precioMedia: 'X', precioEntera: '14.00' },
+  { nombre: 'plato7', alergenos: [12], precioMedia: 'X', precioEntera: '15.00' },
   { nombre: 'plato8', alergenos: [12, 3], precioMedia: 'X', precioEntera: '11.50' },
-  { nombre: 'plato9', alergenos: [2, 3, 12], precioMedia: 'X', precioEntera: '12.00' },
+  { nombre: 'plato9', alergenos: [2, 3, 12], precioMedia: 'X', precioEntera: '14.00' },
   { nombre: 'plato10', alergenos: [1, 3, 7, 12, 2], precioMedia: 'X', precioEntera: '20.00' },
   { nombre: 'plato11', alergenos: [3], precioMedia: 'X', precioEntera: '14.00' }
 ]
